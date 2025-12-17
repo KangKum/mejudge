53ef8ed60d2443843fe82443e0864f2c2fbfcb39
KangKum <130296731+KangKum@users.noreply.github.com>
2025-12-17 12:24:15 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // ← 백엔드 주소
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
