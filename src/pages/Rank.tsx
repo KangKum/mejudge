cc03fbb76f799bc67d99fc3ffe8ace7e3b191a0f
KangKum <130296731+KangKum@users.noreply.github.com>
2025-12-17 14:53:10 +0900

    rank1

--- src/pages/Rank.tsx
+++ src/pages/Rank.tsx
@@ -39,10 +39,10 @@ const Rank = () => {
       <div className="blankSpace h-8 md:h-12"></div>
       <div className="w-full h-full flex flex-col justify-center items-center">
         <h1 className="text-sm md:text-base font-bold mb-4 text-gray-600">오전 3시마다 갱신됩니다</h1>
-        <div className="flex w-full h-12 justify-center items-center">
-          <div className="w-[15%] h-full flex justify-center items-center md:text-xl text-lg border">순위</div>
-          <div className="w-[60%] h-full flex justify-center items-center md:text-xl text-lg border">닉네임</div>
-          <div className="w-[25%] h-full flex justify-center items-center md:text-xl text-lg border">
+        <div className="flex w-full h-12 justify-center items-center gap-1">
+          <div className="w-[15%] h-full flex justify-center items-center md:text-xl text-lg border-b">순위</div>
+          <div className="w-[60%] h-full flex justify-center items-center md:text-xl text-lg border-b">닉네임</div>
+          <div className="w-[25%] h-full flex justify-center items-center md:text-xl text-lg border-b">
             <FaThumbsUp />
           </div>
         </div>
@@ -52,7 +52,7 @@ const Rank = () => {
           <div className="w-full overflow-y-auto overflow-x-hidden">
             {rankings.map((user, index) => (
               <div key={index} className="flex w-full h-10 justify-center items-center">
-                <div className="w-[15%] h-full flex justify-center items-center">{index + 1}</div>
+                <div className="w-[15%] h-full flex justify-center items-center">{index + 1}위</div>
                 <div className="w-[60%] h-full flex justify-center items-center">{user.nickname}</div>
                 <div className="w-[25%] h-full flex justify-center items-center">{user.totalLikes}개</div>
               </div>
