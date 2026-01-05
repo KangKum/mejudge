e87dff7a88ad6647a0f73d5cf4c4979c46fd80a7
KangKum <130296731+KangKum@users.noreply.github.com>
2026-01-06 01:18:04 +0900

    fix homePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,13 +18,13 @@ const Home = () => {
       <div className="w-[85%] md:w-[60%] md:min-w-[800px] md:min-h-[600px] flex flex-col items-center">
         <div className="blankSpace h-[150px]"></div>
         <span className="homeTitle text-5xl md:text-8xl">내가 판사라면</span>
-        <div className="blankSpace h-[120px]"></div>
+        <div className="blankSpace h-[100px]"></div>
         <span className="homeSub1 w-[300px] md:w-[550px] text-base md:text-xl md:text-center">
           실제 사건을 재구성한 가상 사건을 통해 직접 형량을 판단해보고, 이를 실제 판결과 비교할 수 있는 플랫폼입니다.
         </span>
-        <div className="blankSpace h-[100px]"></div>
+        <div className="blankSpace h-10"></div>
         <span className="homeSub2 text-xs md:text-sm">※본 플랫폼은 국가 기관과 무관한 독립적인 서비스입니다.</span>
-        <div className="blankSpace h-[100px]"></div>
+        <div className="blankSpace h-20"></div>
         <button
           className="homeButton md:rounded-xl rounded-lg bg-[#e5e0e0] p-2 md:p-4 text-base md:text-2xl text-black hover:cursor-pointer"
           onClick={() => navigate("/main")}
@@ -33,7 +33,7 @@ const Home = () => {
         </button>
         <div className="blankSpace h-3"></div>
         <span className="homeSub2 text-xs md:text-sm text-gray-400">15세 이상 권장</span>
-        <div className="blankSpace h-[100px]"></div>
+        <div className="blankSpace h-15"></div>
         <div className="flex w-[85%] md:w-[35%] justify-between">
           <button onClick={() => setShowAskForm(true)}>고객센터</button>
           <button onClick={() => setShowPrivacyForm(true)}>개인정보처리방침</button>
