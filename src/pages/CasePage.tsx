d668ea45207e79fe59850cb6094eb857779635fa
KangKum <130296731+KangKum@users.noreply.github.com>
2026-01-10 01:28:18 +0900

    bounce15

--- src/pages/CasePage.tsx
+++ src/pages/CasePage.tsx
@@ -393,8 +393,8 @@ const CasePage = () => {
   }, [userId, caseId]);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center overflow-y-auto">
-      <div className="w-[90%] md:w-[55%] flex flex-col">
+    <div className="w-full h-full flex flex-col justify-center items-center ">
+      <div className="w-[90%] md:w-[55%] flex flex-col overflow-y-auto">
         <div className="w-full h-8 mb-2 flex justify-between items-center mt-4">
           <button className={`text-sm md:text-base ${caseNumber === 1 ? "text-gray-500" : "active:font-bold"}`} onClick={async () => pastCase()}>
             이전 사건
